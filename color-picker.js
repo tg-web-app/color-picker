5f764fc864f328c2d2fab80fc3f517209e0c1e56
crawlic-stud <nikitosik0726@gmail.com>
2022-11-21 00:32:26 +0300

    test colors

---
+++ color-picker.js
@@ -0,0 +1,55 @@
+var currentColor = document.getElementById("bg");
+
+function selectColor(button) {
+    button.style.outline = "7px solid"; 
+    button.style.outlineColor = getComputedStyle(document.querySelector(":root")).getPropertyValue("--tg-theme-button-color"); 
+    currentColor.style.outline = "2px solid"; 
+    currentColor.style.outlineColor = getComputedStyle(document.querySelector(":root")).getPropertyValue("--tg-theme-secondary-bg-color"); 
+    currentColor = button;
+}
+
+function initColorPicker() {
+    var canvas = document.getElementById('color-picker');
+    
+    let ctx = canvas.getContext('2d', { willReadFrequently: true })
+    let gradient = ctx.createLinearGradient(0, 0, canvas.width, 0)
+    gradient.addColorStop(0, '#ff0000')
+    gradient.addColorStop(1 / 6, '#ffff00')
+    gradient.addColorStop((1 / 6) * 2, '#00ff00')
+    gradient.addColorStop((1 / 6) * 3, '#00ffff')
+    gradient.addColorStop((1 / 6) * 4, '#0000ff')
+    gradient.addColorStop((1 / 6) * 5, '#ff00ff')
+    gradient.addColorStop(1, '#ff0000')
+    ctx.fillStyle = gradient
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+  
+    gradient = ctx.createLinearGradient(0, 0, 0, canvas.height)
+    gradient.addColorStop(0.02, 'rgba(255, 255, 255, 1)')
+    gradient.addColorStop(0.5, 'rgba(255, 255, 255, 0)')
+    gradient.addColorStop(1, 'rgba(255, 255, 255, 0)')
+    ctx.fillStyle = gradient
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+  
+    gradient = ctx.createLinearGradient(0, 0, 0, canvas.height)
+    gradient.addColorStop(0, 'rgba(0, 0, 0, 0)')
+    gradient.addColorStop(0.5, 'rgba(0, 0, 0, 0)')
+    gradient.addColorStop(1, 'rgba(0, 0, 0, 1)')
+    ctx.fillStyle = gradient
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+  
+    canvas.onclick = function(e) {
+        const x = (e.offsetX / canvas.clientWidth) * canvas.width;
+        const y = (e.offsetY / canvas.clientHeight) * canvas.height;
+        let imgData = ctx.getImageData(x, y, 1, 1);
+        let rgba = imgData.data;
+        let color = "rgba(" + rgba[0] + ", " + rgba[1] + ", " + rgba[2] + ", " + rgba[3] + ")";
+        let cursor = document.getElementById("cursor");
+        cursor.style.background = color;
+        cursor.style.visibility = "visible";
+        cursor.style.left = `${e.clientX - 10}px`;
+        cursor.style.top = `${e.clientY - 10}px`;
+        currentColor.style.background = color;
+    }
+  }
+  
+  initColorPicker();
