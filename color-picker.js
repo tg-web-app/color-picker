b8fcbe35f46965c024e0017c651bdf31c3c0070d
crawlic-stud <nikitosik0726@gmail.com>
2022-11-21 00:50:44 +0300

    color changes 2

--- color-picker.js
+++ color-picker.js
@@ -4,7 +4,7 @@ function selectColor(button) {
     let selectColor = getComputedStyle(document.querySelector(":root")).getPropertyValue("--tg-theme-button-color");
     let unselectColor = getComputedStyle(document.querySelector(":root")).getPropertyValue("--tg-theme-secondary-bg-color"); 
     button.style.boxShadow = `0px 0px 0px 5px ${selectColor}`; 
-    currentColor.style.boxShadow = `0px 0px 0px 2px ${selectColor}`;
+    currentColor.style.boxShadow = `0px 0px 0px 2px ${unselectColor}`;
     currentColor = button;
 }
 
